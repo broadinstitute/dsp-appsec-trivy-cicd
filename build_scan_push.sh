1cbc6cb42b022272301fa18e56a71d32dfbd570f
Zybjana Bedo <zbedo@broadinstitute.org>
2020-04-02 19:56:59 -0400

    Edit script

--- build_scan_push.sh
+++ build_scan_push.sh
@@ -1,18 +1,21 @@
 set -eu
 
-
 if [[  -z "${1+set}" ]]; then
-    echo "Please enter docker image name" ;
+    echo "Please enter docker IMAGE NAME" ;
     echo "Example: sh build_scan_push.sh YOUR_IMAGE_NAME"  
 else 
     image="$1"
+    project=$( gcloud config get-value project)
+    tag="latest"
 
     echo "Building image ..."
-    docker build -t us.gcr.io/${GOOGLE_PROJECT}/$image:latest . 
+    docker build -t $image:latest . 
+
+    docker tag $image:$tag us.gcr.io/$project/$image:$tag
 
     echo "Scanning image ..."
-    docker run --rm -v $HOME/Library/Caches:/root/.cache/ aquasec/trivy  --exit-code 1 --severity CRITICAL,HIGH us.gcr.io/${GOOGLE_PROJECT}/$image:latest 
+    docker run --rm -v $HOME/Library/Caches:/root/.cache/ aquasec/trivy  --exit-code 1 --severity CRITICAL us.gcr.io/$project/$image:$tag
 
     echo "Pushing image to GCR ..."
-    gcloud docker -- push us.gcr.io/${GOOGLE_PROJECT}/$image:latest 
+    gcloud docker -- push us.gcr.io/$project/$image:$tag
 fi
